e36959691bfc0122c340a915d257d7591c005ba3
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-04-05 10:34:04 +0800

    python最基础操作v0.0

---
+++ myfirst.py
@@ -0,0 +1,38 @@
+#单行注释
+'''
+多行注释1
+多行注释2
+多行注释3
+'''
+
+#用户输入函数
+print("请输入你的名字")
+name=input()
+print("你的名字是",name)
+
+#查看python保留字
+import keyword
+print(keyword.kwlist)
+
+#查看变量数据类型
+a=1
+b=3.16 
+c=True 
+print(type(a))
+print(type(b))
+print(type(c))
+
+#代数运算
+print(15/4)		#输出3.75
+print(15//4)	#整除运算：输出3
+print(2**3)		#乘方运算：输出8
+
+#字符串
+##单引号包裹
+print('What\'s your name?')	#单引号要加转义字符\
+##双引号包裹
+print("What's your name?")	#单引号可以自由使用
+###三引号包裹：可以表示多行字符串 （可以自由使用单双引号）
+print('''the first line
+the second line
+the third line''')
