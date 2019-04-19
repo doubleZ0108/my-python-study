8a21497c50f414b44e8f0a6988bf26087caef3cf
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-04-19 20:51:22 +0800

    使用PyQt5库创建窗口 (三个点击按钮 消息窗口/计算器/播放音乐)

--- PyQt5/myWindow.py
+++ PyQt5/myWindow.py
@@ -1,6 +1,7 @@
 import sys
+import os
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import *
 
 from myQt import *
 
@@ -9,9 +10,16 @@ class mywindow(QtWidgets.QMainWindow,Ui_MainWindow):
         super(mywindow,self).__init__()
         self.setupUi(self)
 
+
     #定义槽函数
-    def myButtonClick(self):
+    def messboxBtnClick(self):
         QtWidgets.QMessageBox.information(self.pushButton,"标题","这是第一个PyQt5 GUI程序")
+
+    def calcBtnClick(self):
+        os.system("C:\\Windows\\System32\\calc.exe")
+
+    def musicBtnClick(self):
+        os.startfile(r"Resources\CHINA-2.mp3")
 
 
 if __name__ == '__main__':
