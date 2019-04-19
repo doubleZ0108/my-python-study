4ed3d161a0448f4bd43cff71aaae4c9160665a80
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-04-19 20:50:37 +0800

    .ui文件转换成的.py文件 (三个pushbutton)

--- PyQt5/myQt.py
+++ PyQt5/myQt.py
@@ -4,17 +4,22 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
 
-    def myButtonClick(self):
-        QtWidgets.QMessageBox.information(self.pushButton, "这里是标题", "这里是一个消息框")
-
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(170, 120, 211, 51))
+        self.pushButton.setGeometry(QtCore.QRect(200, 50, 141, 51))
         self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(200, 120, 141, 51))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(QtCore.QRect(200, 190, 141, 51))
+        self.pushButton_3.setObjectName("pushButton_3")
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 18))
@@ -25,7 +30,9 @@ class Ui_MainWindow(object):
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
-        self.pushButton.clicked.connect(MainWindow.myButtonClick)
+        self.pushButton.clicked.connect(MainWindow.messboxBtnClick)
+        self.pushButton_2.clicked.connect(MainWindow.calcBtnClick)
+        self.pushButton_3.clicked.connect(MainWindow.musicBtnClick)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
 
@@ -33,7 +40,10 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("double Z", "double Z"))
-        self.pushButton.setText(_translate("MainWindow", "这是一个点击按钮"))
+        self.pushButton.setText(_translate("MainWindow", "弹出一个对话框"))
+        self.pushButton_2.setText(_translate("MainWindow", "打开计算器"))
+        self.pushButton_3.setText(_translate("MainWindow", "播放音乐"))
+
 
 
 
