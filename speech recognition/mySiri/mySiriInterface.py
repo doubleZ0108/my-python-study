20f71dd7be6f55dbc0d6e5caf13016908eec888b
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-04-22 22:57:35 +0800

    mySiri界面v2.0

--- speech recognition/mySiri/mySiriInterface.py
+++ speech recognition/mySiri/mySiriInterface.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'untitled.ui'
-#
-# Created by: PyQt5 UI code generator 5.12.1
-#
-# WARNING! All changes made in this file will be lost!
-
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QMovie
 
@@ -16,12 +10,16 @@ class Ui_MainWindow(object):
         MainWindow.setStyleSheet("background-color: rgb(0, 0, 0);")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+
+        self.flag = False
+
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(20, 200, 381, 31))
         font = QtGui.QFont()
         font.setFamily("Calibri")
         font.setPointSize(14)
         self.label.setFont(font)
+        self.label.setVisible(False)
         self.label.setStyleSheet("color: rgb(0, 117, 210);")
         self.label.setObjectName("label")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
@@ -30,6 +28,7 @@ class Ui_MainWindow(object):
         font.setFamily("Calibri")
         font.setPointSize(14)
         self.label_2.setFont(font)
+        self.label_2.setVisible(False)
         self.label_2.setAutoFillBackground(False)
         self.label_2.setStyleSheet("color: rgb(0, 117, 210);")
         self.label_2.setObjectName("label_2")
@@ -39,6 +38,7 @@ class Ui_MainWindow(object):
         font.setFamily("Calibri")
         font.setPointSize(14)
         self.label_3.setFont(font)
+        self.label_3.setVisible(False)
         self.label_3.setStyleSheet("color: rgb(0, 117, 210);")
         self.label_3.setObjectName("label_3")
         self.label_4 = QtWidgets.QLabel(self.centralwidget)
@@ -47,7 +47,8 @@ class Ui_MainWindow(object):
         font.setFamily("Calibri")
         font.setPointSize(14)
         self.label_4.setFont(font)
-        self.label_4.setStyleSheet("color: rgb(0, 117, 210);")
+        self.label_4.setVisible(False)
+        self.label_4.setStyleSheet("color: rgb(241, 162, 183);")
         self.label_4.setObjectName("label_4")
         self.label_5 = QtWidgets.QLabel(self.centralwidget)
         self.label_5.setGeometry(QtCore.QRect(140, 290, 261, 31))
@@ -55,7 +56,8 @@ class Ui_MainWindow(object):
         font.setFamily("Calibri")
         font.setPointSize(14)
         self.label_5.setFont(font)
-        self.label_5.setStyleSheet("color: rgb(0, 117, 210);")
+        self.label_5.setVisible(False)
+        self.label_5.setStyleSheet("color: rgb(241, 162, 183);")
         self.label_5.setObjectName("label_5")
         self.label_6 = QtWidgets.QLabel(self.centralwidget)
         self.label_6.setGeometry(QtCore.QRect(140, 350, 271, 31))
@@ -63,14 +65,31 @@ class Ui_MainWindow(object):
         font.setFamily("Calibri")
         font.setPointSize(14)
         self.label_6.setFont(font)
-        self.label_6.setStyleSheet("color: rgb(0, 117, 210);")
+        self.label_6.setVisible(False)
+        self.label_6.setStyleSheet("color: rgb(241, 162, 183);")
         self.label_6.setObjectName("label_6")
 
+        self.label_7 = QtWidgets.QLabel(self.centralwidget)
+        self.label_7.setGeometry(QtCore.QRect(10, 190, 401, 51))
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(20)
+        self.label_7.setFont(font)
+        self.label_7.setStyleSheet("color: rgb(0, 117, 210);")
+        self.label_7.setObjectName("label_7")
+        self.label_8 = QtWidgets.QLabel(self.centralwidget)
+        self.label_8.setGeometry(QtCore.QRect(50, 250, 381, 41))
+        font = QtGui.QFont()
+        font.setFamily("Calibri")
+        font.setPointSize(11)
+        self.label_8.setFont(font)
+        self.label_8.setStyleSheet("color: rgb(223, 175, 135);")
+        self.label_8.setObjectName("label_8")
 
         self.siriGif = QtWidgets.QLabel(self.centralwidget)
         self.siriGif.setGeometry(QtCore.QRect(70, 450, 271, 131))
         self.siriGif.setText("")
-        self.gif = QMovie("Resources/icon/siri1.gif")
+        self.gif = QMovie("Resources/icon/siri.gif")
         self.siriGif.setMovie(self.gif)
         self.gif.start()
         self.siriGif.setScaledContents(True)
@@ -97,6 +116,8 @@ class Ui_MainWindow(object):
         self.label_4.setText(_translate("MainWindow", "Play music"))
         self.label_5.setText(_translate("MainWindow", "Open Notepad"))
         self.label_6.setText(_translate("MainWindow", "Open the calculator"))
+        self.label_7.setText(_translate("MainWindow", "\"Hey Kerr\" to wake me!"))
+        self.label_8.setText(_translate("MainWindow", "Double Click me to know more!"))
 
 
 import Resources_rc
