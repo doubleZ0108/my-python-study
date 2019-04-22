b2479bd23ab198329111eb72a14a2966a4e94af5
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-04-22 22:56:58 +0800

    mySiri逻辑部分v2.0

--- speech recognition/mySiri/mySiri.py
+++ speech recognition/mySiri/mySiri.py
@@ -1,5 +1,4 @@
 import sys
-import os
 
 from PyQt5.QtWidgets import *
 
@@ -13,11 +12,25 @@ class mywindow(QtWidgets.QMainWindow,Ui_MainWindow):
 
     #定义槽函数
     def mouseDoubleClickEvent(self, event):
-        os.system("C:\\Windows\\System32\\calc.exe")
+        self.label_7.setVisible(self.flag)
+        self.label_8.setVisible(self.flag)
+
+        # 交替显示
+        if self.flag==True:
+            self.flag=False
+        else:
+            self.flag=True
+
+        # 操作指南 开/关
+        self.label.setVisible(self.flag)
+        self.label_2.setVisible(self.flag)
+        self.label_3.setVisible(self.flag)
+        self.label_4.setVisible(self.flag)
+        self.label_5.setVisible(self.flag)
+        self.label_6.setVisible(self.flag)
 
 
 if __name__ == '__main__':
-
     app = QApplication(sys.argv)
     window = mywindow()
     window.show()
