55d50852e6aa154d8ebadc1a2018566121e2f406
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-04-23 23:54:48 +0800

    mySiri逻辑部分v5.0

--- speech recognition/mySiri/mySiri.py
+++ speech recognition/mySiri/mySiri.py
@@ -1,8 +1,9 @@
 import sys, threading, time, difflib, os, random
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QIcon
 import speech_recognition as sr
 
-from untitled import *
+from mySiriInterface import *
 
 r = sr.Recognizer()
 
@@ -14,13 +15,14 @@ class mywindow(QtWidgets.QMainWindow,Ui_MainWindow):
     def __init__(self):
         super(mywindow,self).__init__()
         self.setupUi(self)
+        self.setWindowIcon(QIcon('Resources/icon/siri.png'))
 
     # 语音识别函数
     def siri_recognition(self):
 
         # 在定时器执行函数内部重复构造定时器
         global timer
-        timer = threading.Timer(5.1, self.siri_recognition)  # 之后是5s执行一次
+        timer = threading.Timer(5.1, self.siri_recognition)  # 之后是5.1s执行一次
         timer.start()
         print("Time clock...")
 
@@ -43,7 +45,7 @@ class mywindow(QtWidgets.QMainWindow,Ui_MainWindow):
             print("The similar is ", similar)
 
             if similar > 0.1 or string_similar(command, "what") > 0.5:    # 第二个条件是为了提高唤醒概率(测试用)
-                if self.flag==False:        # 如果此时正在看帮助信息则识别不成功
+                if self.flag == False:        # 如果此时正在看帮助信息则识别不成功
                     self.WakeSuccess()
                 else:
                     self.WakeFailed()
@@ -52,9 +54,9 @@ class mywindow(QtWidgets.QMainWindow,Ui_MainWindow):
 
 
     def mouseDoubleClickEvent(self, event):
-        if self.label_10.isVisible()==True:
+        if self.label_10.isVisible()==True:     # 当Kerr已经被唤醒时, 取消双击事件 => 直接返回
             return
-        
+
         self.label_7.setVisible(self.flag)
         self.label_8.setVisible(self.flag)
 
@@ -63,7 +65,7 @@ class mywindow(QtWidgets.QMainWindow,Ui_MainWindow):
         if self.flag == True:
             self.flag = False
             # 打开定时器
-            timer = threading.Timer(0, self.siri_recognition)
+            timer = threading.Timer(0.1, self.siri_recognition)
             timer.start()
             print("==============Timer is start==============")
         else:
@@ -91,6 +93,7 @@ class mywindow(QtWidgets.QMainWindow,Ui_MainWindow):
 
     def WakeSuccess(self):
         print("wake success!")
+
         global timer
         timer.cancel()
         self.label_7.setVisible(False)
@@ -126,7 +129,7 @@ class mywindow(QtWidgets.QMainWindow,Ui_MainWindow):
                 self.label_12.setVisible(False)
 
             if max_index == 0:
-                os.startfile(r"Resources\CHINA-2.mp3")
+                os.startfile(r"Resources\music\CHINA-2.mp3")
             elif max_index == 1:
                 os.system("C:\\Windows\\System32\\notepad.exe")
             elif max_index == 2:
@@ -134,13 +137,13 @@ class mywindow(QtWidgets.QMainWindow,Ui_MainWindow):
             else:
                 os.startfile("https://github.com/doubleZ0108/")
 
-
+        time.sleep(2.1)
 
         self.label_7.setVisible(True)
         self.label_8.setVisible(True)
         self.label_10.setVisible(False)
         self.label_11.setVisible(False)
-        timer = threading.Timer(0, self.siri_recognition)
+        timer = threading.Timer(0.1, self.siri_recognition)
         timer.start()
 
     def WakeFailed(self):
@@ -154,11 +157,7 @@ if __name__ == '__main__':
     window = mywindow()
     window.show()
 
-    timer = threading.Timer(0, window.siri_recognition)  # 第一次执行0s后开始
+    timer = threading.Timer(0.1, window.siri_recognition)  # 第一次执行0.1s后开始
     timer.start()
-
-    # time.sleep(15)  # 15s之后停止定时器
-    # print("Timer is end...")
-    # timer.cancel()
 
     sys.exit(app.exec())
