121862b7601eb90117e0f152b2e6fe2209107bdc
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-05-24 09:38:22 +0800

    python基础语法之列表生成器

--- list-study.py
+++ list-study.py
@@ -25,3 +25,37 @@ reverse_sorted_list = sorted(list, reverse=True)
 len_sorted_list = sorted(list, key=len)	# 根据字符串的长度进行排序而不是默认的字母表顺序
 
 list.sort()	# 直接在原对象上进行排序
+
+
+
+
+import sys
+
+## 列表的生成式语法，此方法需要消耗较多的内存
+f = [x for x in range(1,10)]
+print(f)
+f = [x+y for x in 'ABCDE' for y in '1234567']
+print(f)
+
+f = [x**2 for x in range(1,10)]
+print(f)
+print(sys.getsizeof(f))		# 查看对象占用内存的字节数
+
+## 创建生成器对象，可以获取到数据但它不占用额外的空间存储数据，每次需要用到数据的时候就再次计算得到（需要花费额外的时间）
+f = (x**2 for x in range(1,10))
+print(sys.getsizeof(f))
+print(f)
+for val in f:
+	print(val)
+	
+	
+## 将函数改造成生成器函数
+# 斐波那契数列
+def fib(n):
+	a,b = 0,1
+	for _ in range(n):
+		a,b = b,a+b
+		yield a
+		
+for val in fib(20):
+	print(val)
