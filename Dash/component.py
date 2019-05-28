89e57fd3372040f89ea18631bd5735e912b95c57
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-05-28 20:08:03 +0800

    dash小控件

---
+++ Dash/component.py
@@ -0,0 +1,57 @@
+# 下拉框
+html.Label('Dropdown'),
+dcc.Dropdown(
+    options=[
+        {'label': 'New York City', 'value': 'NYC'},
+        {'label': u'Montréal', 'value': 'MTL'},
+        {'label': 'San Francisco', 'value': 'SF'}
+    ],
+    value='MTL'
+),
+
+# 复选下拉框
+html.Label('Multi-Select Dropdown'),
+dcc.Dropdown(
+    options=[
+        {'label': 'New York City', 'value': 'NYC'},
+        {'label': u'Montréal', 'value': 'MTL'},
+        {'label': 'San Francisco', 'value': 'SF'}
+    ],
+    value=['MTL', 'SF'],
+    multi=True
+),
+
+# 单选框
+html.Label('Radio Items'),
+dcc.RadioItems(
+    options=[
+        {'label': 'New York City', 'value': 'NYC'},
+        {'label': u'Montréal', 'value': 'MTL'},
+        {'label': 'San Francisco', 'value': 'SF'}
+    ],
+    value='MTL'
+),
+
+# 复选框
+html.Label('Checkboxes'),
+dcc.Checklist(
+    options=[
+        {'label': 'New York City', 'value': 'NYC'},
+        {'label': u'Montréal', 'value': 'MTL'},
+        {'label': 'San Francisco', 'value': 'SF'}
+    ],
+    values=['MTL', 'SF']
+),
+
+# 输入框
+html.Label('Text Input'),
+dcc.Input(value='MTL', type='text'),
+
+# 滑动条
+html.Label('Slider'),
+dcc.Slider(
+    min=0,
+    max=9,
+    marks={i: 'Label {}'.format(i) if i == 1 else str(i) for i in range(1, 6)},
+    value=5,
+),
