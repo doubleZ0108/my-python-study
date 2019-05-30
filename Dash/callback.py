1b37dd930c1c60e5978ea88a08580538be4099cd
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-05-30 15:02:13 +0800

    dash callback (拖动slider图表相应的变换)

--- Dash/callback.py
+++ Dash/callback.py
@@ -1,17 +1,11 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
 from dash.dependencies import Input, Output
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+# 实时输入输出
 app.layout = html.Div([
     dcc.Input(id='my-id', value='initial value', type='text'),
     html.Div(id='my-div')
 ])
-
 
 @app.callback(
     Output(component_id='my-div', component_property='children'),
@@ -21,5 +15,40 @@ def update_output_div(input_value):
     return 'You\'ve entered "{}"'.format(input_value)
 
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+# 拖动slider图表相应的变换
+app.layout = html.Div([
+    dcc.Graph(id='graph-with-slider'),
+    dcc.Slider(id='year-slider')
+])
+
+@app.callback(
+    Output('graph-with-slider', 'figure'),
+    [Input('year-slider', 'value')])
+def update_figure(selected_year):
+    filtered_df = df[df.year == selected_year]
+    traces = []
+    for i in filtered_df.continent.unique():
+        df_by_continent = filtered_df[filtered_df['continent'] == i]
+        traces.append(go.Scatter(
+            x=df_by_continent['gdpPercap'],
+            y=df_by_continent['lifeExp'],
+            text=df_by_continent['country'],
+            mode='markers',
+            opacity=0.7,
+            marker={
+                'size': 15,
+                'line': {'width': 0.5, 'color': 'white'}
+            },
+            name=i
+        ))
+
+    return {
+        'data': traces,
+        'layout': go.Layout(
+            xaxis={'type': 'log', 'title': 'GDP Per Capita'},
+            yaxis={'title': 'Life Expectancy', 'range': [20, 90]},
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+            legend={'x': 0, 'y': 1},
+            hovermode='closest'
+        )
+    }
