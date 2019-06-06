7dde5c46cba57be9dfa059e64d84d0feb01492ec
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-06-06 16:26:39 +0800

    Google Play Dash v7.0 (add 注释)

--- Dash/GooglePlay Dash/app.py
+++ Dash/GooglePlay Dash/app.py
@@ -1,21 +1,14 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_daq as daq
+from dash.dependencies import Input, Output, State
+
 import pandas as pd
 import numpy as np
-import copy
 import plotly.graph_objs as go
-import dash_daq as daq
 
-from dash.dependencies import Input, Output, State
 from Reader import *
-# from Reader import read_file
-# from Reader import file_filter
-# from Reader import rating_filter
-# from Reader import get_main
-# from Reader import get_size
-# from Reader import get_content_rating
-# from Reader import get_price
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -27,39 +20,40 @@ tab_style = {
     'borderBottom': '1px solid #d6d6d6',
     'padding': '6px',
     'fontWeight': 'bold'
-}
+    }
 tab_selected_style = {
     'borderTop': '1px solid #d6d6d6',
     'borderBottom': '1px solid #d6d6d6',
     'backgroundColor': '#119DFF',
     'color': 'white',
     'padding': '6px'
-}
+    }
 
+# pd方式读取csv文件
 df = pd.read_csv('dataset\google-play-store-apps\googleplaystore.csv')
-
+# 获取软件分类列表
 available_indicators = df['Category'].unique()
 
+# csv.reader方式读取csv文件
 file = read_file()
 
+
 app.layout = html.Div([
-    html.Div(
-        [
-            html.Div(
-                [
+    html.Div([
+            html.Div([
                     # Catagory下拉框
                     dcc.Dropdown(id='catagory',
                                  options=[{
                                      'label': i,
                                      'value': i
-                                 } for i in available_indicators],
+                                 } for i in available_indicators],  # 下拉框的项为软件分类列表
                                  value='ART_AND_DESIGN'),
                     # Price单选按钮
                     dcc.RadioItems(id='price',
                                    options=[{
                                        'label': i,
                                        'value': i
-                                   } for i in ['Free', 'Paid', 'All']],
+                                   } for i in ['Free', 'Paid', 'All']], # 单选按钮的项为 免费/付费/全部
                                    value='All',
                                    labelStyle={'display': 'inline-block'})
                 ],
@@ -69,19 +63,20 @@ app.layout = html.Div([
                 }),
 
             # tab -> 可以切换两个视图
-            html.Div(
-                [
+            html.Div([
                     dcc.Tabs(
                         id="tabs",
                         children=[
+                            # tab1 => 软件大小 & 应用分级
                             dcc.Tab(label='Size & Content Rating',
                                     style=tab_style,
                                     selected_style=tab_selected_style,
                                     children=[
-                                        html.Div(
-                                            [
+                                        html.Div([
+                                                # 软件大小折线图
                                                 dcc.Graph(id='size-graph',
                                                           animate=True),
+                                                # 应用分级饼状图
                                                 dcc.Graph(
                                                     id='content-rating-graph',
                                                     animate=True)
@@ -93,15 +88,17 @@ app.layout = html.Div([
                                                 'padding': '0 0 0 0'
                                             })
                                     ]),
+                            # tab2 => 软件价格 & 评分
                             dcc.Tab(
                                 label='Price & Rating',
                                 style=tab_style,
                                 selected_style=tab_selected_style,
                                 children=[
-                                    html.Div(
-                                        [
+                                    html.Div([
+                                            # 软件大小折线图
                                             dcc.Graph(id='price-graph',
                                                       animate=True),
+                                            # 评分仪表盘
                                             daq.Knob(id='my-knob',
                                                      label="Rating",
                                                      labelPosition='bottom',
@@ -114,20 +111,21 @@ app.layout = html.Div([
                                                      }),
                                             # 仪表盘的callback显示
                                             html.Div(id='knob-output',
-                                            style={
-                                                'marginTop': -80,
-                                                'marginLeft': 425
-                                            }),
-                                            html.Img(id='star-img',
-                                            src='https://upload-images.jianshu.io/upload_images/12014150-c2ee9fe0591b3247.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240',
-                                            height='50px',
-                                            width='50px',
-                                            style={
-                                                'marginTop': -120,
-                                                'marginLeft': 375
-                                            }
-                                            )
-                                        
+                                                     style={
+                                                         'marginTop': -80,
+                                                         'marginLeft': 425
+                                                     }),
+                                            # star图标
+                                            html.Img(
+                                                id='star-img',
+                                                src=
+                                                'https://upload-images.jianshu.io/upload_images/12014150-c2ee9fe0591b3247.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/1240',
+                                                height='50px',
+                                                width='50px',
+                                                style={
+                                                    'marginTop': -120,
+                                                    'marginLeft': 375
+                                                })
                                         ],
                                         style={
                                             'display': 'inline-block',
@@ -150,56 +148,45 @@ app.layout = html.Div([
             'backgroundColor': 'rgb(250, 250, 250)',
             'padding': '10px 5px'
         }),
-    html.Div([dcc.Graph(id='main-graph', animate=True)],
-             style={
-                 'width': '49%',
-                 'display': 'inline-block',
-                 'padding': '0 20'
-             }),
+    
+    html.Div([
+            # main点状图
+            dcc.Graph(id='main-graph', 
+            animate=True
+        )],
+            style={
+                'width': '49%',
+                'display': 'inline-block',
+                'padding': '0 20'
+            }),
 ])
 
 
-# 筛选数据
-def filter(catagory_name, price_choice):
-    # 按照是否付费筛选数据
-    if price_choice == "Free":
-        df_ = df[df['Price'] == "0"]
-    elif price_choice == "Paid":
-        df_ = df[df['Price'] != "0"]
-    else:
-        df_ = df
 
-    # 按照分类筛选数据
-    dff = df_[df_['Category'] == catagory_name]
-
-    return dff
-
-
-# 主点状图 callback
+# main点状图 callback
 @app.callback(
-    dash.dependencies.Output('main-graph', 'figure'),
+    dash.dependencies.Output('main-graph', 'figure'),   # 输出 -> main点状图
     [
-        dash.dependencies.Input('catagory', 'value'),  # 输入1 -> 软件分类
-        dash.dependencies.Input('price', 'value'),
-        dash.dependencies.Input('my-knob', 'value')
+        dash.dependencies.Input('catagory', 'value'),   # 输入1 -> 软件分类
+        dash.dependencies.Input('price', 'value'),      # 输入2 -> 软件是否收费
+        dash.dependencies.Input('my-knob', 'value')     # 输入3 -> 评分仪表盘
     ])  # 输入2 -> 价格
 def update_maingraph(catagory_name, price_choice, rating_value):
 
-    dff = filter(catagory_name, price_choice)
-
+    # 获取评论数, 下载量, 软件名称
     newfile = read_file()
     newfile = file_filter(newfile, catagory_name, price_choice)
     newfile = rating_filter(newfile, rating_value)
 
     [Reviews, Installs, App] = get_main(newfile)
-    # print(size_catagory, size_list)
+    # print(Reviews, Installs, App)
 
     return {
         'data': [
             go.Scatter(
-                x=Reviews,  # 横轴为评论数
-                y=Installs,  # 纵轴为安装数
-                text=App,
+                x=Reviews,      # x轴为评论数
+                y=Installs,     # y轴为安装数
+                text=App,       # 点信息为App的名字
                 mode='markers',
                 marker={
                     'size': 15,
@@ -207,102 +194,113 @@ def update_maingraph(catagory_name, price_choice, rating_value):
                     'line': {
                         'width': 0.5,
                         'color': 'white'
-                    }
-                })
-        ],
+                        }
+                })],
         'layout':
-        go.Layout(xaxis={
-            'title': 'Reviews',
-        },
-                  yaxis={
-                      'title': 'Installs',
-                  },
-                  margin={
-                      'l': 40,
-                      'b': 30,
-                      't': 10,
-                      'r': 0
-                  },
-                  height=450,
-                  hovermode='closest')
-    }
+            go.Layout(
+                xaxis={
+                    'title': 'Reviews',
+                    },
+                yaxis={
+                    'title': 'Installs',
+                    },
+                margin={
+                    'l': 40,
+                    'b': 30,
+                    't': 10,
+                    'r': 0
+                    },
+                height=450,
+                hovermode='closest')
+        }
 
 
 # size折线图 callback
-@app.callback(dash.dependencies.Output('size-graph', 'figure'), [
-    dash.dependencies.Input('catagory', 'value'),
-    dash.dependencies.Input('price', 'value')
-])
+@app.callback(
+    dash.dependencies.Output('size-graph', 'figure'),   # 输出 -> size折线图
+    [
+        dash.dependencies.Input('catagory', 'value'),   # 输入1 -> 软件分类
+        dash.dependencies.Input('price', 'value')       # 输入2 -> 软件是否收费
+    ])
 def update_sizeGraph(catagory_name, price_choice):
 
+    # 获取软件大小分类, 软件大小列表
     newfile = read_file()
     newfile = file_filter(newfile, catagory_name, price_choice)
+
     [size_catagory, size_list] = get_size(newfile)
     # print(size_catagory, size_list)
 
     return {
         'data': [{
-            'x': size_catagory,
-            'y': size_list,
+            'x': size_catagory,     # x轴为软件大小分类
+            'y': size_list,         # y轴为软件大小列表
             'type': 'Scatter',
-        }],
+            }],
         'layout':
-        go.Layout(margin={
-            'l': 40,
-            'b': 50,
-            't': 20,
-            'r': 20
-        },
-                  legend={
-                      'x': 0,
-                      'y': 1
-                  },
-                  height=280,
-                  hovermode='closest')
-    }
+            go.Layout(
+                margin={
+                    'l': 40,
+                    'b': 50,
+                    't': 20,
+                    'r': 20
+                    },
+                legend={
+                    'x': 0,
+                    'y': 1
+                    },
+                height=280,
+                hovermode='closest')
+        }
 
 
 # content rating饼状图 callback
-@app.callback(dash.dependencies.Output('content-rating-graph', 'figure'), [
-    dash.dependencies.Input('catagory', 'value'),
-    dash.dependencies.Input('price', 'value')
-])
+@app.callback(
+    dash.dependencies.Output('content-rating-graph', 'figure'),     # 输出 -> content rating饼状图
+    [
+        dash.dependencies.Input('catagory', 'value'),               # 输入1 -> 软件分类
+        dash.dependencies.Input('price', 'value')                   # 输入2 -> 软件是否收费
+    ])
 def update_conten_tratingGraph(catagory_name, price_choice):
 
+    # 获取应用分级分类, 应用分级列表
     newfile = read_file()
     newfile = file_filter(newfile, catagory_name, price_choice)
 
-    [content_rating_catagory,
-     content_rating_list] = get_content_rating(newfile)
+    [content_rating_catagory, content_rating_list] = get_content_rating(newfile)
     # print(content_rating_catagory,content_rating_list)
 
-    trace = go.Pie(
-        labels=content_rating_catagory,
-        values=content_rating_list,
-    )
 
+    trace = go.Pie(
+        labels=content_rating_catagory,     # x轴为应用分级分类
+        values=content_rating_list,         # y轴为应用分级列表
+        )
     return {
         'data': [trace],
         'layout':
-        go.Layout(margin={
-            'l': 130,
-            'b': 30,
-            't': 50,
-            'r': 0
-        },
-                  height=300,
-                  hovermode='closest')
-    }
+            go.Layout(
+                margin={
+                    'l': 130,
+                    'b': 30,
+                    't': 50,
+                    'r': 0
+                    },
+                height=300,
+                hovermode='closest')
+        }
 
 
 # price折线图 callback
-@app.callback(dash.dependencies.Output('price-graph', 'figure'), [
-    dash.dependencies.Input('catagory', 'value'),
-    dash.dependencies.Input('price', 'value'),
-    dash.dependencies.Input('my-knob', 'value')
-])
+@app.callback(
+    dash.dependencies.Output('price-graph', 'figure'),  # 输出 -> price折线图
+    [
+        dash.dependencies.Input('catagory', 'value'),   # 输入1 -> 软件分类
+        dash.dependencies.Input('price', 'value'),      # 输入2 -> 软件是否收费
+        dash.dependencies.Input('my-knob', 'value')     # 输入3 -> 评分仪表盘
+    ])
 def update_priceGraph(catagory_name, price_choice, rating_value):
 
+    # 获取软件价格分类, 软件价格列表
     newfile = read_file()
     newfile = file_filter(newfile, catagory_name, price_choice)
     newfile = rating_filter(newfile, rating_value)
@@ -310,32 +308,34 @@ def update_priceGraph(catagory_name, price_choice, rating_value):
     [price_catagory, price_list] = get_price(newfile)
 
     return {
-        'data': [
-            {
-                "x": price_catagory,
-                "y": price_list,
-                "mode": "lines+markers",
-                'type': 'Scatter',
-                'name': 'Line'
-            },
-        ],
+        'data': [{
+            "x": price_catagory,        # x轴为软件价格分类
+            "y": price_list,            # y轴为软件价格列表
+            "mode": "lines+markers",
+            'type': 'Scatter',
+            'name': 'Line'
+            }],
         'layout':
-        go.Layout(margin={
-            'l': 130,
-            'b': 50,
-            't': 50,
-            'r': 40
-        },
-                  height=300,
-                  hovermode='closest')
-    }
+            go.Layout(
+                margin={
+                    'l': 130,
+                    'b': 50,
+                    't': 50,
+                    'r': 40
+                    },
+                height=300,
+                hovermode='closest')
+        }
 
 
-# 仪表盘 callback
-@app.callback(dash.dependencies.Output('knob-output', 'children'),
-              [dash.dependencies.Input('my-knob', 'value')])
+# 评分仪表盘 callback
+@app.callback(
+    dash.dependencies.Output('knob-output', 'children'), # 输出 -> 仪表盘文字
+    [dash.dependencies.Input('my-knob', 'value')])       # 输入 -> 评分仪表盘
 def update_output(value):
-    return '<= {}.'.format(value)
+    return '<= {}'.format(value)
+
+
 
 
 if __name__ == '__main__':
