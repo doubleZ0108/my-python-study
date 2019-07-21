c1f174b2316858aa9438b225c9be8a94845c630e
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-07-21 15:03:46 +0800

    day6: 多进程复习

---
+++ 100Days/Review/6.py
@@ -0,0 +1,20 @@
+from multiprocessing import Process,Lock
+from time import time, sleep
+from random import randint
+
+def Download_Task(filename):
+    print('%s下载开始...' % filename)
+    time_to_download = randint(1,10)
+    sleep(time_to_download)
+    print('%s下载完成, 耗时%d' % (filename, time_to_download))
+
+if __name__ == "__main__":
+    start = time()
+    p1 = Process(target=Download_Task, args=('file1',))
+    p1.start()
+    p2 = Process(target=Download_Task, args=('file2',))
+    p2.start()
+    p1.join()
+    p2.join()
+    end = time()
+    print("共耗时%.2f" % (end-start))
