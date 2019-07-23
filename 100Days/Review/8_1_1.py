1f9327b68ba1893ce428805a10d86ad3779da0ec
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-07-23 12:37:23 +0800

    day8: TCP套接字传输文本信息(客户端)

---
+++ 100Days/Review/8_1_1.py
@@ -0,0 +1,10 @@
+from socket import socket
+
+def main():
+    client = socket()
+    client.connect(('192.168.223.1',7890))
+    print(client.recv(1024).decode('utf-8'))
+    client.close()
+
+if __name__ == "__main__":
+    main()
