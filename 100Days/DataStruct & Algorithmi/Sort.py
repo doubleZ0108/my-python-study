948f79a1bb0bd5f9c3ae9c256db06f3c24849543
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-07-25 10:12:36 +0800

    day10: 排序算法

---
+++ 100Days/DataStruct & Algorithmi/Sort.py
@@ -0,0 +1,83 @@
+from random import randint
+
+# 冒泡排序
+def BubbleSort(arr, cmp=lambda x,y: x<y):
+    for i in range(len(arr)-1):
+        swapped = False
+        for j in range(0,len(arr)-i-1):
+            if cmp(arr[j+1],arr[j]):
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+                swapped = True
+        if not swapped:
+            break
+    return arr
+
+# 鸡尾酒排序
+def CocktailSort(arr, cmp=lambda x,y: x<y):
+    for i in range(len(arr)-1):
+        swapped = False
+        for j in range(0,len(arr)-i-1):
+            if cmp(arr[j+1],arr[j]):
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+                swapped = True
+
+        if swapped:
+            swapped = False
+            for j in range(len(arr)-i-2,i,-1):
+                if cmp(arr[j],arr[j-1]):
+                    arr[j-1],arr[j] = arr[j],arr[j-1]
+                    swapped = True
+        
+        if not swapped:
+            break
+
+# 选择排序
+def SelectSort(arr, cmp=lambda x,y: x<y):
+    for i in range(len(arr)-1):
+        minindex = i
+        for j in range(i+1, len(arr)):
+            if cmp(arr[j],arr[minindex]):
+                minindex = j
+        if minindex != i:
+            arr[minindex],arr[i] = arr[i],arr[minindex]
+    return arr
+
+# 归并排序
+def MergeSort(arr, cmp=lambda x,y: x<y):
+    if len(arr)<2:
+        return arr[:]
+    mid = len(arr)//2
+    left = MergeSort(arr[:mid], cmp)
+    right = MergeSort(arr[mid:], cmp)
+    return merge(left, right, cmp)
+
+def merge(arr1, arr2, cmp):
+    merged_arr = []
+    index1,index2 = 0,0
+    while index1<len(arr1) and index2<len(arr2):
+        if cmp(arr1[index1],arr2[index2]):
+            merged_arr.append(arr1[index1])
+            index1 += 1
+        else:
+            merged_arr.append(arr2[index2])
+            index2 += 1
+
+    merged_arr += arr1[index1:]
+    merged_arr += arr2[index2:]
+    return merged_arr
+
+
+if __name__ == "__main__":
+    MaxSize = 10
+    arr = [0]*MaxSize
+    for i in range(len(arr)):
+        arr[i] = randint(0,100)
+
+    # sorted_arr = BubbleSort(arr)
+    # sorted_arr = CocktailSort(arr)
+    # sorted_arr = SelectSort(arr)
+    # sorted_arr = MergeSort(arr)
+
+    for arg in sorted_arr:
+        print(arg, end=' ')
+    print()
