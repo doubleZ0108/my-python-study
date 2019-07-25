2acdc536770165b27311a730d1349a52184721e5
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-07-25 10:33:47 +0800

    day10: 排序算法

--- 100Days/DataStruct & Algorithmi/Sort.py
+++ 100Days/DataStruct & Algorithmi/Sort.py
@@ -66,6 +66,34 @@ def merge(arr1, arr2, cmp):
     merged_arr += arr2[index2:]
     return merged_arr
 
+# 快速排序
+def QuickSort(arr):
+    arr = quick_sort(arr,0,len(arr)-1)
+    return arr
+
+def quick_sort(arr, low, high):
+    if low>= high:
+        return arr
+
+    i,j = low, high
+    key = arr[low]
+    while low < high:
+        while low<high and arr[high]>=key:
+            high -= 1
+        if arr[high]<key:
+            arr[low],arr[high] = arr[high],arr[low]
+            low += 1
+        while low<high and arr[low]<=key:
+            low += 1
+        if arr[low]>key:
+            arr[low],arr[high] = arr[high],arr[low]
+            high -= 1
+    arr[low] = key
+
+    quick_sort(arr,i,low-1)
+    quick_sort(arr,low+1,j)
+    return arr
+
 
 if __name__ == "__main__":
     MaxSize = 10
@@ -77,6 +105,8 @@ if __name__ == "__main__":
     # sorted_arr = CocktailSort(arr)
     # sorted_arr = SelectSort(arr)
     # sorted_arr = MergeSort(arr)
+    # sorted_arr = QuickSort(arr)
+
 
     for arg in sorted_arr:
         print(arg, end=' ')
