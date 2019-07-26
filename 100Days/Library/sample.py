b8b789ae8e3ff3cea6753773e10372f76ea39eab
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-07-26 13:49:50 +0800

    day11: sample -> 从列表中选择不重复的n个元素

--- 100Days/Library/sample.py
+++ 100Days/Library/sample.py
@@ -1,10 +1,5 @@
 from random import sample
 
-## enumerate -> 既遍历出数值又遍历出索引
-li = [54,12,6,8,5,23,1]
-for index,item in enumerate(li):
-    print(index, item)
-
 ## sample -> 从列表中选择不重复的n个元素
 nums = [x for x in range(1,11)]
 for i in range(6):
