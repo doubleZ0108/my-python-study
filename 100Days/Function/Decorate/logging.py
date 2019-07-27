6ecfe45fe6acd2f114458496213f4ad248a73422
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-07-27 20:36:13 +0800

    dat12: 函数装饰器 -> 日志(带参数的装饰器)

--- 100Days/Function/Decorate/logging.py
+++ 100Days/Function/Decorate/logging.py
@@ -2,13 +2,27 @@
 ## 日志
 from functools import wraps
 
-def logging(fn):
-    @wraps(fn)
-    def log_decorate(*args, **kwargs):
-        print(fn.__name__ + ' was called')
-        return fn(*args, **kwargs)
-    return log_decorate
+def logging(logfile='log.txt'):
+    def logging_decorate(fn):
+        @wraps(fn)
+        def wrapped_func(*args, **kwargs):
+            log_str = fn.__name__ + " was called"
+            with open(logfile,'a') as f:
+                f.write(log_str + '\n')
+            return fn(*args, **kwargs)
+        return wrapped_func
+    return logging_decorate
 
-@logging
-def func(a,b):
-    print(a,b)
+@logging()  # 写入默认日志文件(这个小括号是必须的)
+def myfunc1(a,b):
+    c = a + b
+    print(c)
+
+myfunc1(1,2)
+
+@logging('myfunc2_log.txt')
+def myfunc2(a,b):
+    c = a * b
+    print(c)
+    
+myfunc2(1,2)
