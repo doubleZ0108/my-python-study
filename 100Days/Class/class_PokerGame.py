ff02c98648cbf9dd872d5789142bc7f8b44afb6a
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-07-28 14:25:25 +0800

    day13: 扑克游戏(add 枚举类)

--- 100Days/Class/class_PokerGame.py
+++ 100Days/Class/class_PokerGame.py
@@ -1,11 +1,21 @@
-import random
+from random import shuffle
+from enum import Enum, unique
+
+@unique
+class Suite(Enum):
+    """花色"""
+    
+    SPADE, HEART, CLUB, DIAMOND = range(4)
+
+    def __lt__(self, other):    # 判断self对象是否小于other对象
+        return self.value<other.value
 
 class Card(object):
-    '''一张扑克牌'''
+    """一张牌"""
     def __init__(self, suite, face):
         self._suite = suite
         self._face = face
-    
+
     @property
     def suite(self):
         return self._suite
@@ -15,53 +25,35 @@ class Card(object):
         return self._face
 
     def __str__(self):
-        if self._face == 1:
-            face_str = 'A'
-        elif self._face == 11:
-            face_str = 'J'
-        elif self._face == 12:
-            face_str = 'Q'
-        elif self._face == 13:
-            face_str = 'K'
-        else:
-            face_str = str(self._face)
-
-        return '%s%s' % (self._suite, face_str)
+        suites = ['♠️', '♥️', '♣️', '♦️']
+        faces = ['', 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        return f'{suites[self._suite.value]} {faces[self._face]}'
 
     def __repr__(self):
         return self.__str__()
 
 class Poker(object):
-    '''一副牌'''   
+    """一副牌"""
     def __init__(self):
-        self._cards = [
-            Card(suite, face) 
-            for suite in '♠♥♣♦' 
-            for face in range(1,14)
-        ]   
+        self._card = [Card(suite,face) for suite in Suite for face in range(1,14)]
         self._current = 0
-
-    @property
-    def cards(self):
-        return self._cards
 
     def shuffle(self):
-        '''洗牌(随机排序)'''
         self._current = 0
-        random.shuffle(self._cards)
-
-    @property
-    def next(self):
-        '''发牌'''
-        card = self._cards[self._current]
-        self._current += 1
-        return card
+        shuffle(self._card)
 
     @property
     def has_next(self):
-        return self._current < len(self._cards)
+        return self._current < len(self._card)
+
+    def next(self):
+        card = self._card[self._current]
+        self._current += 1
+        return card
+    
 
 class Player(object):
+    """玩家"""
     def __init__(self, name):
         self._name = name
         self._cards_on_hand = []
@@ -69,20 +61,28 @@ class Player(object):
     @property
     def name(self):
         return self._name
-
+        
     @property
     def cards_on_hand(self):
         return self._cards_on_hand
 
-    def get(self, card):
-        '''摸牌'''
+    def get_one(self, card):
         self._cards_on_hand.append(card)
 
-    def arrange(self, card_key):
-        '''整理手上的牌'''
-        self._cards_on_hand.sort(key = card_key)
+    def sort(self, cmp=lambda card: (card.suite, card.face)):
+        self._cards_on_hand.sort(key=cmp)
 
-def get_key(card):
-    return (card.suite, card.face)
+def main():
+    poker = Poker()
+    poker.shuffle()
+    players = [Player('东邪'),Player('西毒'),Player('南帝'),Player('北丐')]
+    while poker.has_next:
+        for player in players:
+            player.get_one(poker.next())
+    for player in players:
+        player.sort()
+        print(player.name,end=': ')
+        print(player.cards_on_hand)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    main()
