a156360cbe7f75830f60a8205fae4b369a46fc10
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-09-09 20:33:26 +0800

    将一组图片转换为gif

--- 100Days/Image/Images2Gif.py
+++ 100Days/Image/Images2Gif.py
@@ -4,14 +4,31 @@ Resources/Result/目录下有多张图片
 '''
 import os
 from PIL import Image
+import imageio
 
 dirname = 'Resources/Result/'
 frames = list(filter(lambda x: x[0:5]=='frame' and x[-4:]=='.png', os.listdir(dirname)))    # 将不符合命名要求的图片过滤掉(MacOS)会默认创建一些文件
 frames.sort(key=lambda x: int(x[5:-4]))     # 按照图片编号进行排序
 
-imgs = []
-for frame in frames:
-    img = Image.open(dirname+frame)
-    imgs.append(img)
+'''
+imageio库
+'''
+def myImage2Gif_imageio(dirname):
+    frames = list(filter(lambda x: x[0:5] == 'frame' and x[-4:] == '.png', os.listdir(dirname)))  # 将不符合命名要求的图片过滤掉(MacOS)会默认创建一些文件
+    frames.sort(key=lambda x: int(x[5:-4]))     # 按照图片编号进行排序
 
-imgs[0].save(dirname + 'result.gif', save_all=True, append_images=imgs, duration=5)
+    imgs = []
+    for frame in frames:
+        imgs.append(imageio.imread(dirname+frame))
+    imageio.mimsave(dirname+'result.gif', imgs, 'GIF', duration=0.35)
+
+'''
+有点莫名其妙，有时候会只在首位两张图片之间闪烁
+'''
+def myImage2Gif_PIL():
+    imgs = []
+    for frame in frames:
+        img = Image.open(dirname+frame)
+        imgs.append(img)
+
+    imgs[0].save(dirname + 'result.gif', save_all=True, append_images=imgs, duration=5)
