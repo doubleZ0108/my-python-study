47537d7bbd4591ede4273c59d4cc75571984968c
double Z <39005525+doubleZ0108@users.noreply.github.com>
2019-12-08 19:50:08 +0800

    js向python传递数据并接收返回值

--- Eel/server/server.py
+++ Eel/server/server.py
@@ -7,17 +7,29 @@
 
 @create: 2019/12/02 
 '''
-
 import eel
 
+'''处理js传来的数据'''
 @eel.expose
-def my_python_func(data):       # 使用json传递数据
+def my_python_func_receive_data(data):       # 使用json传递数据
     print('my python function...')
     print(data)
     username, password = data['username'], data['password']
     print(username, password)
 
 
+'''处理js传来的数据并返回'''
+@eel.expose
+def my_python_func_send_data(origin_data):
+    print('从js中传入的原始数据' + origin_data)
+    username, password = 'doubleZ', '77777'
+    data = {
+        'username': username,
+        'password': password
+    }
+    return data
+
+
 if __name__ == '__main__':
-    eel.init('../web/')
-    eel.start('index.html')
+    eel.init('../web/')         # 存放静态网页的根目录
+    eel.start('index.html')     # 启动server时的初始界面
--- Eel/web/js/main.js
+++ Eel/web/js/main.js
@@ -1,6 +1,5 @@
-
-
-function my_js_func() {
+/* js向python发送数据 */
+function my_js_func_send_data() {
     console.log('my js function...');
     let username_info = 'doubleZ';
     let password_info = '1234';
@@ -11,5 +10,19 @@ function my_js_func() {
 
     console.log('调用my_python_function');
 
-    eel.my_python_func(data);       //使用json传递数据
+    eel.my_python_func_receive_data(data);       //使用json传递数据
+}
+
+
+/* js向python传数据并得到返回值 */
+async function my_js_func_receive_data(){
+    let origin_data = "111";
+
+    let data = await my_js_func_receive_data_async(origin_data);
+    console.log(data, data.username, data.password);
+}
+
+async function my_js_func_receive_data_async(origin_data){
+    let data = eel.my_python_func_send_data(origin_data)();        //这里必须用两个括号
+    return data;
 }
